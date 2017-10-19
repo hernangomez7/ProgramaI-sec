ee7b59a077a4e400eb2e24ce4b7b66cf634a5393
hernangomez7 <dath_math@hotmail.com>
2017-10-18 21:25:30 -0300

    Add files via upload

--- clase17/mem-dy/main.c
+++ clase17/mem-dy/main.c
@@ -5,16 +5,30 @@
 
 int main()
 {
-    Empleado * pEmpleado;
-    pEmpleado = empleado_newConstructor("JUAN","PEREZ",44);
-    empleado_debugShow(pEmpleado);
+    int i;
+    Empleado * pEmpleado[50];
+
+    void *pGenerico[50];//puntero generico, guarda direcciones de memoria no importa el tipo
+
+    for(i=0;i<50;i++)
+    {
+        //pEmpleado[i] = empleado_newConstructor("JUAN","PEREZ",i);
+        pGenerico[i] = empleado_newConstructor("JUAN","PEREZ",i);
+    }
+
+    empleado_debugShow((Empleado*)pGenerico[33]);//acepta Empleado no tipo void
+
+    /*
+    empleado_debugShow(pEmpleado[33]);
+    empleado_debugShow(pEmpleado[42]);
     empleado_setNombre(pEmpleado,"CARLOS");
     empleado_debugShow(pEmpleado);
     empleado_setApellido(pEmpleado,"AP");
     empleado_debugShow(pEmpleado);
     empleado_setidSector(pEmpleado,87);
     empleado_debugShow(pEmpleado);
+    printf("EL NOMBRE ES: %s",empleado_getnombre(pEmpleado));*/
 
-    printf("EL NOMBRE ES: %s",empleado_getnombre(pEmpleado));
+
     return 0;
 }
